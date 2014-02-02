a88c772076d7d830d8f8a8b0919235b5e0bb11a4
John Sundell <john.sundell@me.com>
2014-02-03 00:14:47 +0100

    Add SSKMultiplatform
    This is a utility header that will be used by
    various SuperSpriteKit classes & categories. It
    provides macros mapped to platform-specific types,
    as well as abstractions that allows the user to
    handle platform-specific features (like device
    orientation) in a platform-agnostic manner.

---
+++ SSKMultiplatform.h
@@ -0,0 +1,85 @@
+#ifndef SSKMultiplatform
+#define SSKMultiplatform
+
+#pragma mark - Types
+
+#if TARGET_OS_IPHONE
+#define SSKWindowType UIWindow
+#define SSKViewControllerType UIViewController
+#define SSKFontType UIFont
+#define SSKEdgeInsetsType UIEdgeInsets
+#else
+#define SSKWindowType NSWindow
+#define SSKViewControllerType NSViewController
+#define SSKFontType NSFont
+#define SSKEdgeInsetsType NSEdgeInsets
+#endif
+
+#pragma mark - Platform information
+
+/**
+ *  Enum describing various platforms that a SpriteKit game can run on
+ */
+typedef enum : NSUInteger {
+    SSKPlatformTypePhone,
+    SSKPlatformTypePad,
+    SSKPlatformTypeDesktop
+} SSKPlatformType;
+
+/**
+ *  Get the current platform type that the game is currently running on
+ */
+static inline SSKPlatformType SSKCurrentPlatform()
+{
+#if TARGET_OS_IPHONE
+    return UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ? SSKPlatformTypePhone : SSKPlatformTypePad;
+#else
+    return SSKPlatformTypeDesktop;
+#endif
+}
+
+#pragma mark - Device orientation information
+
+/**
+ *  Enum providing a simplified, platform-agnostic way to handle device orientation
+ */
+typedef enum : NSUInteger {
+    SSKDeviceOrientationPortrait,
+    SSKDeviceOrientationLandscape
+} SSKDeviceOrientation;
+
+/**
+ *  Get the current orientation of the device that the game is currently running on
+ *
+ *  @discussion For OSX, this will always return SSKDeviceOrientationLandscape
+ */
+static inline SSKDeviceOrientation SSKCurrentDeviceOrientation()
+{
+#if TARGET_OS_IPHONE
+    if (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation))
+        return SSKDeviceOrientationLandscape;
+    
+    return SSKDeviceOrientationPortrait;
+#else
+    return SSKDeviceOrientationLandscape;
+#endif
+}
+
+#pragma mark - Screen scale information
+
+/**
+ *  Get the scale of the screen that the game is currently running on
+ *
+ *  @discussion For OSX, this will return the scale of the screen on which the
+ *  application's key window is being rendered.
+ */
+static inline CGFloat SSKCurrentScreenScale()
+{
+#if TARGET_OS_IPHONE
+    return [UIScreen mainScreen].scale;
+#else
+    return [[NSScreen mainScreen] backingScaleFactor];
+#endif
+}
+
+#endif
