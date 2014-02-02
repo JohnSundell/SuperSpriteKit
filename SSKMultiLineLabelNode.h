27e472e641937ef37382c68cf5428effcf99922f
John Sundell <john.sundell@me.com>
2014-02-03 00:37:06 +0100

    Add SSKMultiLineLabelNode
    This label node is capable of rendering multiple
    lines of text. While it currently doesn't provide
    an API as extensive as UILabel for example, it
    provides a simple API that will hopefully fill the
    needs of basic game UIs.

---
+++ SSKMultiLineLabelNode.h
@@ -0,0 +1,72 @@
+#import <SpriteKit/SpriteKit.h>
+#import "SSKMultiplatform.h"
+
+/**
+ *  A label node capable of rendering multiple lines of text
+ *
+ *  This class depends on the SSKMultiplatform header
+ */
+@interface SSKMultiLineLabelNode : SKNode
+
+/**
+ *  The name of the font to use when rendering the node's text
+ */
+@property (nonatomic, copy) NSString *fontName;
+
+/**
+ *  The font size to use when rendering the node's text
+ */
+@property (nonatomic) CGFloat fontSize;
+
+/**
+ *  The font color to use when rendering the node's text
+ */
+@property (nonatomic, strong) SKColor *fontColor;
+
+/**
+ *  The text the node is displaying
+ *
+ *  @discussion Setting this property will cause the node to re-render its text.
+ */
+@property (nonatomic, copy) NSString *text;
+
+/**
+ *  The total size of the node
+ */
+@property (nonatomic, readonly) CGSize size;
+
+/**
+ *  Allocate and initialize a new instance of JSMultiLineLabelNode
+ *
+ *  @param fontName The name of the font to use when rendering the node's text.
+ *  @param fontSize The font size to use when rendering the node's text.
+ *  @param maximumWidth The maximum width the node should have. When a line of
+ *  text has reached the maximum width, the text will be wrapped to a new line.
+ *  @param text The text the node should display.
+ */
++ (instancetype)multiLineLabelNodeWithFontNamed:(NSString *)fontName
+                                       fontSize:(CGFloat)fontSize
+                                   maximumWidth:(CGFloat)maximumWidth
+                                           text:(NSString *)text;
+
+/**
+ *  Allocate and initialize a new instance of JSMultiLineLabelNode
+ *
+ *  @param fontName The name of the font to use when rendering the node's text.
+ *  @param fontSize The font size to use when rendering the node's text.
+ *  @param fontColor The font color to use when rednering the node's text.
+ *  @param numberOfLines The maximum number of lines the node should have.
+ *  When the maximum number of lines has been reached, the node will stop
+ *  rendering text.
+ *  @param maximumWidth The maximum width the node should have. When a line of
+ *  text has reached the maximum width, the text will be wrapped to a new line.
+ *  @param text The text the node should display.
+ */
++ (instancetype)multiLineLabelNodeWithFontNamed:(NSString *)fontName
+                                       fontSize:(CGFloat)fontSize
+                                      fontColor:(SKColor *)fontColor
+                                  numberOfLines:(NSUInteger)numberOfLines
+                                   maximumWidth:(CGFloat)maximumWidth
+                                           text:(NSString *)text;
+
+@end
---
+++ SSKMultiLineLabelNode.m
@@ -0,0 +1,186 @@
+#import "SSKMultiLineLabelNode.h"
+
+static CGFloat JSMultilineLabelNodeGetFontLineHeight(SSKFontType *font)
+{
+#if TARGET_OS_IPHONE
+    return font.lineHeight;
+#else
+    return [[NSLayoutManager new] defaultLineHeightForFont:font];
+#endif
+}
+
+static NSString *JSMultilineLabelNodeGetStringForWord(NSString *word)
+{
+    return [word stringByAppendingString:@" "];
+}
+
+@interface SSKMultiLineLabelNode()
+
+@property (nonatomic, strong) NSArray *lineLabelNodes;
+@property (nonatomic) NSUInteger numberOfLines;
+@property (nonatomic) CGFloat maximumWidth;
+
+@end
+
+@implementation SSKMultiLineLabelNode
+
++ (instancetype)multiLineLabelNodeWithFontNamed:(NSString *)fontName fontSize:(CGFloat)fontSize maximumWidth:(CGFloat)maximumWidth text:(NSString *)text
+{
+    return [self multiLineLabelNodeWithFontNamed:fontName
+                                        fontSize:fontSize
+                                       fontColor:[SKColor blackColor]
+                                   numberOfLines:0
+                                    maximumWidth:maximumWidth
+                                            text:text];
+}
+
++ (instancetype)multiLineLabelNodeWithFontNamed:(NSString *)fontName fontSize:(CGFloat)fontSize fontColor:(SKColor *)fontColor numberOfLines:(NSUInteger)numberOfLines maximumWidth:(CGFloat)maximumWidth text:(NSString *)text
+{
+    SSKMultiLineLabelNode *label = [SSKMultiLineLabelNode node];
+    
+    label.numberOfLines = numberOfLines;
+    label.fontName = fontName;
+    label.fontSize = fontSize;
+    label.fontColor = fontColor;
+    label.maximumWidth = maximumWidth;
+    label.text = text;
+    
+    return label;
+}
+
+- (void)setFontSize:(CGFloat)fontSize
+{
+    if (_fontSize == fontSize) {
+        return;
+    }
+    
+    _fontSize = fontSize;
+    
+    [self drawLineLabelNodes];
+}
+
+- (void)setFontColor:(SKColor *)fontColor
+{
+    if (!fontColor) {
+        return;
+    }
+    
+    if ([_fontColor isEqual:fontColor]) {
+        return;
+    }
+    
+    _fontColor = fontColor;
+    
+    for (SKLabelNode *labelNode in self.lineLabelNodes) {
+        labelNode.fontColor = fontColor;
+    }
+}
+
+- (void)setText:(NSString *)text
+{
+    if ([_text isEqualToString:text]) {
+        return;
+    }
+    
+    _text = text;
+    
+    [self drawLineLabelNodes];
+}
+
+- (void)drawLineLabelNodes
+{
+    [self.lineLabelNodes makeObjectsPerformSelector:@selector(removeFromParent)];
+    self.lineLabelNodes = nil;
+    
+    if ([self.text length] == 0 || self.maximumWidth <= 0) {
+        return;
+    }
+    
+    NSString *text = [self.text stringByReplacingOccurrencesOfString:@"\n" withString:@"\n "];
+    
+    NSMutableArray *lineLabelNodes = [NSMutableArray new];
+    NSArray *wordsInText = [text componentsSeparatedByString:@" "];
+    NSUInteger numberOfParsedWords = 0;
+    
+    SSKFontType *font = [SSKFontType fontWithName:self.fontName size:self.fontSize];
+    CGFloat fontLineHeight = JSMultilineLabelNodeGetFontLineHeight(font);
+    NSDictionary *textAttributes = @{NSFontAttributeName: font};
+    NSCharacterSet *newLineCharacterSet = [NSCharacterSet newlineCharacterSet];
+    
+    while (numberOfParsedWords < [wordsInText count]) {
+        NSMutableString *lineText = [NSMutableString string];
+        
+        while ([lineText sizeWithAttributes:textAttributes].width < self.maximumWidth) {
+            if (numberOfParsedWords == [wordsInText count]) {
+                break;
+            }
+            
+            NSString *word = [wordsInText objectAtIndex:numberOfParsedWords];
+            NSArray *wordLines = [word componentsSeparatedByCharactersInSet:newLineCharacterSet];
+            
+            if ([wordLines count] > 1) {
+                NSRange newLineCharacterRange = [word rangeOfCharacterFromSet:newLineCharacterSet];
+                word = [word stringByReplacingCharactersInRange:newLineCharacterRange withString:@""];
+            }
+            
+            [lineText appendString:JSMultilineLabelNodeGetStringForWord(word)];
+            numberOfParsedWords++;
+            
+            if ([wordLines count] > 1) {
+                break;
+            }
+        }
+        
+        while ([lineText sizeWithAttributes:textAttributes].width > self.maximumWidth) {
+            NSString *lastWord = JSMultilineLabelNodeGetStringForWord([wordsInText objectAtIndex:numberOfParsedWords - 1]);
+            NSRange deleteRange = NSMakeRange([lineText length] - [lastWord length], [lastWord length]);
+            [lineText deleteCharactersInRange:deleteRange];
+            numberOfParsedWords--;
+        }
+        
+        SKLabelNode *lineLabelNode = [SKLabelNode labelNodeWithFontNamed:self.fontName];
+        lineLabelNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentModeLeft;
+        lineLabelNode.verticalAlignmentMode = SKLabelVerticalAlignmentModeBaseline;
+        lineLabelNode.fontSize = self.fontSize;
+        lineLabelNode.fontColor = self.fontColor;
+        lineLabelNode.text = lineText;
+        
+        [self addChild:lineLabelNode];
+        [lineLabelNodes addObject:lineLabelNode];
+        
+        if (self.numberOfLines > 0 && self.numberOfLines == [lineLabelNodes count]) {
+            break;
+        }
+    }
+    
+    NSUInteger lineNumber = 1;
+    NSUInteger numberOfLines = [lineLabelNodes count];
+    
+    for (SKLabelNode *labelNode in lineLabelNodes) {
+        CGPoint labelPosition;
+        labelPosition.x = 0;
+        labelPosition.y = (numberOfLines - lineNumber) * fontLineHeight;
+        labelNode.position = labelPosition;
+        
+        lineNumber++;
+    }
+    
+    self.lineLabelNodes = lineLabelNodes;
+}
+
+- (CGSize)size
+{
+    CGSize size;
+    size.width = 0;
+    size.height = CGRectGetMaxY([[self.lineLabelNodes firstObject] frame]);
+
+    for (SKLabelNode *labelNode in self.lineLabelNodes) {
+        if (CGRectGetMaxX(labelNode.frame) > size.width) {
+            size.width = CGRectGetMaxX(labelNode.frame);
+        }
+    }
+    
+    return size;
+}
+
+@end
