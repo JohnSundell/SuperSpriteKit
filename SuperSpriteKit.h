4cab92a183f9e8e1ebbc83c8b24ca941c37c9eac
John Sundell <john.sundell@me.com>
2014-02-22 12:56:55 +0100

    Add SuperSpriteKit.h import header

---
+++ SuperSpriteKit.h
@@ -0,0 +1,11 @@
+#import <SpriteKit/SpriteKit.h>
+
+#import "SSKMultiplatform.h"
+
+#import "SKNode+SSKTags.h"
+#import "SKSpriteNode+SSKAnimation.h"
+
+#import "SSKInteractionHandler.h"
+#import "SSKMultiLineLabelNode.h"
+#import "SSKTileableNode.h"
+#import "SSKStretchableNode.h"
